49c0f4288a1da70cd4926f00fe4d9d904469682d
Tim Denning <36028402+denningta@users.noreply.github.com>
2023-05-22 18:41:35 +0200

    fixed Dashboard header to include multiple options for displaying the user's name

--- pages/dashboard/index.tsx
+++ pages/dashboard/index.tsx
@@ -9,26 +9,48 @@ import AgentDetails from '../../components/widgets/AgentDetails'
 import WellPermitsAssignment from '../../components/widgets/WellPermitsAssignment/WellPermitsAssignment'
 import { tailwindBreakpoints } from '../../lib/tailwindcss/tailwindConfig'
 import useTailwindBreakpoints from '../../hooks/useTailwindBreakpoints'
+import useAgentInfo from '../../hooks/useAgentInfo'
 
 const Dashboard: NextPageWithLayout = () => {
   const { user } = useUser()
+  const { data } = useAgentInfo(user?.sub)
   const breakpoint = useTailwindBreakpoints()
 
+  console.log(data)
+
+
+  let title: string = 'Hello'
+  let name: string | undefined = undefined
+
+
+  if (user) {
+    name = user && (user.given_name as string ?? user.name)
+    title = `Hello, ${name}`
+  }
+
+  if (data && data.firstName && data.lastName) {
+    name = `${data.firstName} ${data.lastName}`
+    title = `Hello, ${name}`
+  }
+
+
+
+
   const widgets: Widget[] = [
-    { 
-      component: <Header 
-        title={`Hello, ${user && user.given_name}`}
+    {
+      component: <Header
+        title={title}
         subtitle='Your wells at a glance'
-      />, 
+      />,
       colspan: 3
     },
-    { 
-      component: <PermitPreview />, 
-      colspan: breakpoint === 'sm' || breakpoint === 'md' ? 3 : 2 
+    {
+      component: <PermitPreview />,
+      colspan: breakpoint === 'sm' || breakpoint === 'md' ? 3 : 2
     },
-    { 
-      component: <AgentDetails />, 
-      colspan: breakpoint === 'sm' || breakpoint === 'md' ? 3 : 1 
+    {
+      component: <AgentDetails />,
+      colspan: breakpoint === 'sm' || breakpoint === 'md' ? 3 : 1
     },
     { component: <WellPermitsAssignment />, colspan: 3 }
   ]
