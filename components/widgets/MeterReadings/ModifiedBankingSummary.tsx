96a2443438f51602ed183cdb19392cd6fa71cf11
Tim Denning <36028402+denningta@users.noreply.github.com>
2023-10-28 09:43:36 +0200

    added agGrid for the dbb013 summary

--- components/widgets/MeterReadings/ModifiedBankingSummary.tsx
+++ components/widgets/MeterReadings/ModifiedBankingSummary.tsx
@@ -1,8 +1,10 @@
 import { Accordion, AccordionDetails, AccordionSummary } from "@mui/material"
 import useDbb004BankingSummary from "../../../hooks/useDbb004BankingSummary"
 import useTailwindBreakpoints from "../../../hooks/useTailwindBreakpoints"
-import EditButton from "../../common/EditButton"
 import { IoChevronDown } from "react-icons/io5"
+import { ColDef, ICellRendererParams } from "ag-grid-community"
+import { AgGridReact } from "ag-grid-react"
+import styles from "./ModifiedBankingSummary.module.css"
 
 interface Props {
   permitNumber: string | undefined
@@ -13,37 +15,50 @@ const ModifiedBankingSummary = ({ permitNumber, year }: Props) => {
   const { data } = useDbb004BankingSummary(permitNumber, year)
   const breakpoint = useTailwindBreakpoints()
 
-  const title = <div className="font-bold text-xl">Three Year Modified Banking (from DBB-013)</div>
+  const title = <div className="font-bold text-xl">Three Year Modified Banking Summary (from DBB-013)</div>
 
-  const content = <div>
-    <div className="grid grid-cols-6 gap-4 mt-2">
-      {data?.allowedAppropriation
-        ? <div className="col-span-2 font-bold text-right">{data.allowedAppropriation} acre-feet</div>
-        : <div className="col-span-2 text-right text-gray-400">not defined</div>
-      }
-      <div className="col-span-4 font-thin">ALLOWED ANNUAL APPROPRIATION PER APPROVED CHANGE</div>
+  const colDefs: ColDef[] = [
+    {
+      field: 'number',
+      headerName: 'Value',
+      maxWidth: 80,
+      suppressMovable: true,
+      editable: true,
+    },
+    {
+      field: 'description',
+      headerName: 'Description',
+      minWidth: 400,
+      suppressMovable: true,
+    }
 
-      {data?.pumpingLimitThisYear
-        ? <div className="col-span-2 font-bold text-right">{data.pumpingLimitThisYear} acre-feet</div>
-        : <div className="col-span-2 text-right text-gray-400">not defined</div>
-      }
-      <div className="col-span-4 font-thin">ALLOWED PUMPING THIS YEAR WITH 3-YEAR BANKING</div>
+  ]
 
-      {data?.flowMeterLimit
-        ? <div className="col-span-2 font-bold text-right">{data.flowMeterLimit} acre-feet</div>
-        : <div className="col-span-2 text-right text-gray-400">not defined</div>
-      }
-      <div className="col-span-4 font-thin">FLOW METER LIMIT</div>
+  const rowData = [
+    { number: data?.allowedAppropriation, description: "Allowed annual appropriation per approved change" },
+    { number: data?.pumpingLimitThisYear, description: "Allowed pumping this year with 3-year banking" },
+    { number: data?.flowMeterLimit, description: "Flow meter limit" }
+  ]
+
+  const formContent = (
+    <div className={`ag-theme-alpine`} style={{ height: 150, width: 500 }}>
+      <AgGridReact
+        columnDefs={colDefs}
+        rowData={rowData}
+        headerHeight={20}
+        suppressRowClickSelection={true}
+      />
     </div>
-  </div>
+
+  )
 
   return (
     <div className="relative">
       {(breakpoint !== 'sm' && breakpoint !== 'md') ?
-        <>
-          <>{title}</>
-          <>{content}</>
-        </>
+        <div className="space-y-2">
+          <div>{title}</div>
+          <div>{formContent}</div>
+        </div>
         :
         <Accordion>
           <AccordionSummary
@@ -52,7 +67,7 @@ const ModifiedBankingSummary = ({ permitNumber, year }: Props) => {
             {title}
           </AccordionSummary>
           <AccordionDetails>
-            {content}
+            {formContent}
           </AccordionDetails>
         </Accordion>
 
