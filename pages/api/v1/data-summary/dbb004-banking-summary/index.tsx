a974c1d08fcaa319ea670b7f2e548e2bb2479415
Tim Denning <36028402+denningta@users.noreply.github.com>
2023-12-14 10:21:11 +0100

    fixed bug

--- pages/api/v1/data-summary/dbb004-banking-summary/index.tsx
+++ pages/api/v1/data-summary/dbb004-banking-summary/index.tsx
@@ -1,6 +1,7 @@
 import { withApiAuthRequired } from "@auth0/nextjs-auth0";
 import { NextApiRequest, NextApiResponse } from "next";
 import listDbb004BankingSummary from "./list";
+
 import updateDbb004BankingSummary from "./update";
 
 type HandlerFunctions = {
